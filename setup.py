9881dc9aeb613128a073a709b3a5b3cc450d8635
Samargithubb <100934815+Samargithubb@users.noreply.github.com>
2023-05-24 09:05:07 +0530

    requirement created

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguage',
+    version= '0.0.0',
+    author = 'samar',
+    author_email= 'samarrana407@gmail.com',
+    packages=find_packages(),
+    install_requires = []
+
+)
+
+
